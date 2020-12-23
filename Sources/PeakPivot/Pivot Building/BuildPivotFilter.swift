50555a04928e14380050ec8e230d0b7edd3efd13
lukestringer90 <luke@lukestringer.me>
2020-12-23 13:36:45 +0000

    Update access modifiers

--- Sources/PeakPivot/Pivot Building/BuildPivotFilter.swift
+++ Sources/PeakPivot/Pivot Building/BuildPivotFilter.swift
@@ -11,8 +11,8 @@ import Foundation
 /// Describe how to filter a pivot by including only data matching certain fields
 public struct BuildPivotFilter: Codable {
     /// The field name to filter on
-    let fieldName: FieldName
+    public let fieldName: FieldName
     
     /// The values of the field name to include
-    let include: [FieldValue]
+    public let include: [FieldValue]
 }
