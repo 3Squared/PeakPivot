a69971f9cd4ddc79b9dc01865cd0b2dfa8704f72
lukestringer90 <luke@lukestringer.me>
2020-12-23 11:57:10 +0000

    Add storyboard and App Delegate

---
+++ Example/iOS/AppDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.swift
+//  PeakPivot Example (iOS)
+//
+//  Created by Luke Stringer on 23/12/2020.
+//
+
+
+import UIKit
+
+@UIApplicationMain
+
+class AppDelegate: UIResponder, UIApplicationDelegate { }
